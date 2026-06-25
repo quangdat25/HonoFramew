0fd4d002f65cb3daa9a64aa1ed1723b9821d43d4
dquangdat04 <emailcuaban@gmail.com>
2026-06-25 10:21:04 +0700

    hono with react 1

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,8 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/SU26_SDN');
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost:27017/SE1940",
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
